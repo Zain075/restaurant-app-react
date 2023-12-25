37d834965fd5e15324863bbea8abe650638c716e
zain075 <zain75official@gmail.com>
2023-12-25 11:05:52 +0530

    first commit

---
+++ resataraunt-server/index.js
@@ -0,0 +1,24 @@
+//1 import json server
+const jsonServer = require('json-server')
+
+//2 create a server
+const server = jsonServer.create()
+
+//3 setup path for db.json file
+const router = jsonServer.router('db.json')
+
+//4 Return middleware used by json server
+const middleware = jsonServer.defaults()
+
+//5 setup portfor server
+const port = process.env.port || 3001
+
+//6 use middleware and router
+server.use(middleware)
+server.use(router)
+
+//7 to run the server
+server.listen(port,()=>{
+    console.log("listening on the port"+port);
+})
+
